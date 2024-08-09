876fbcc25d958a9e0cfb4439fcb40391f1870178
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-08-09 10:52:22 +0300

    added comments

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,7 @@
+-- creating a table called users
+-- with attributes id, email, name and country
+-- project focusses on making an atrribute unique
+-- directly in a schema to avoid bugs and enforce rules
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
